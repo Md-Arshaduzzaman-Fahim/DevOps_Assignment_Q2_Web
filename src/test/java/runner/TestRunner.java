40017566544b32422eefcf2747c516363ef82a91
Md-Arshaduzzaman-Fahim <fahimxzaman@gmail.com>
2024-09-16 08:18:32 +0600

    updated cucumber bdd

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,14 +8,17 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "src/test/resources/features",
 
 		glue = {"com.qa.ajkerpatrika.stepdefs"},
+				 plugin = {
+			                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+			        },
 		monochrome = true
 		
 		)
 public class TestRunner extends AbstractTestNGCucumberTests{
 	
-	@DataProvider(parallel = true)
-	public Object [][] scenario(){
-		return super.scenarios();
-	}
+//	@DataProvider(parallel = true)
+//	public Object [][] scenario(){
+//		return super.scenarios();
+//	}
 
 }
